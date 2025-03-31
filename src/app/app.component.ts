f2b2e7b60281d8b3011a4fac030cbc6755443932
Antonio Sanchez <cuentapalaswebs@gmail.com>
2025-03-31 23:51:00 +0200

    chore: add transloco and tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { TranslocoModule, TranslocoService } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, TranslocoModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'inv-portfolio-simulator';
+  private readonly translocoService = inject(TranslocoService);
+
+  changeLanguage(lang: string) {
+    this.translocoService.setActiveLang(lang);
+  }
 }
